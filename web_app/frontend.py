664ea1f08d047b16b2b254960c391bfa1611ac44
hectorLop <lopez.almazan.hector@gmail.com>
2022-05-24 20:39:00 +0200

    feature: Added title and description to Gradio

--- web_app/frontend.py
+++ web_app/frontend.py
@@ -40,10 +40,37 @@ def process_tweet(url: str) -> str:
     return outcome
 
 
+title = "Twitter Positivity Analyzer"
+description = """
+<h2> Description </h2>
+Twitter is a social media network on which users post and interact with messages known as "tweets".
+It allows an user to post, like, and retweet tweets.
+
+Twitter is also known by the excessive negativity or criticism by a great part of its users.
+Considering that, this application intends to classify a tweet according to its positivity.
+The positivity is measured in five categories:
+- Extremely negative
+- Negative
+- Neutral
+- Positive
+- Extremely positive
+
+The application is based on a BERT model fine tuned on this
+[dataset](https://www.kaggle.com/datasets/datatattle/covid-19-nlp-text-classification).
+"""
+
+article = "Check out this \
+[repository](https://github.com/hectorLop/Twitter_Positivity_Analyzer) \
+with a lot more details about this method and implementation."
+
+
 app = gr.Interface(
     fn=process_tweet,
     inputs=gr.inputs.Textbox(lines=2, placeholder="Tweet url..."),
     outputs="text",
+    title=title,
+    description=description,
+    article=article,
 )
 
 if __name__ == "__main__":
